6ce03bf65596254513b361a6fd34373e1e52cb5c
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-01-25 00:10:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function getMPDFile() {
-  return "https://linearjitp02-playback.astro.com.my/dash-wv/linear/2504/default.mpd";
+async function getMPDFile() {
+    return "https://linearjitp02-playback.astro.com.my/dash-wv/linear/2504/default.mpd";
 }
 
-function getClearKey() {
-  return "'03c2e0af2f8159f9f0ce9b5dbc865f10': 'cd84ed136b0cc71f8ab8cd4d4f6a2e4c'";
+async function getClearKey() {
+    return {
+        keyId: "03c2e0af2f8159f9f0ce9b5dbc865f10",
+        keyValue: "cd84ed136b0cc71f8ab8cd4d4f6a2e4c"
+    };
 }
